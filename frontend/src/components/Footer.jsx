9636f197be3fc4c7c62d6f3fa0dba2c75d059c17
Sarah Halstead <ehalstead@gmail.com>
2026-02-04 17:30:01 -0800

    Added homepage to the frontend.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy; 2026 - Built with Rathan Kumar's Learn to Build full-fledged Stock Prediction Portal using Python, Django, REST Framework, React.js, and Machine Learning on Udemy.com</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
