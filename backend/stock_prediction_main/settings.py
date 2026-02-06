b5a69fc67d08d617283d1349393bfb998119461f
Sarah Halstead <ehalstead@gmail.com>
2026-02-05 17:38:08 -0800

    added login functionality

--- backend/stock_prediction_main/settings.py
+++ backend/stock_prediction_main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'accounts',
     'api',
     'corsheaders',
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
