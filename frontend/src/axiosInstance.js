4db3d1413d3ccccc62c23be816e848f128167b75
Sarah Halstead <ehalstead@gmail.com>
2026-02-09 16:48:16 -0800

    stock prediction

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -12,7 +12,6 @@ const axiosInstance = axios.create({
 // Request Interceptor
 axiosInstance.interceptors.request.use(
     function(config) {
-        console.log('request without auth header = ', config);
         const accessToken = localStorage.getItem('accessToken')
         if(accessToken) {
             config.headers['Authorization'] = `Bearer ${accessToken}`
